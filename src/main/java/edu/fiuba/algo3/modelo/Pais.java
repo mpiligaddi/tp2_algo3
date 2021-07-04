4dd411924c0fce2c9a397ff05ce64441cd92d791
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-04 13:00:20 -0300

    prueba principal

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo;
+
+public class Pais {
+    int cantidad;
+    String ejercitos;
+    public Pais(int n, String ejercitoActual) {
+        cantidad = n;
+        ejercitos = ejercitoActual;
+    }
+}
