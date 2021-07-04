84b86153e28a63ec9f66cdbab6f7d48cf3a2e6ec
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-04 11:44:33 -0300

    Agrego clase pais

--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -1,2 +1,10 @@
-package edu.fiuba.algo3.modelo;public class Pais {
+package edu.fiuba.algo3.modelo;
+
+public class Pais {
+    int cantidadSoldados;
+    String ejercito;
+    public Pais(int n, String ejercitoActual) {
+        cantidadSoldados = n;
+        ejercito = ejercitoActual;
+    }
 }
