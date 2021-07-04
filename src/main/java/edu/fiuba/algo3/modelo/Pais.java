b3dcec5a07b3acd8093c726767edbe836bbfbd4b
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-04 19:23:28 -0300

    segundo cambio

--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -2,9 +2,7 @@ package edu.fiuba.algo3.modelo;
 
 public class Pais {
     int cantidad;
-    String ejercitoActualesI;
     public Pais(int n, String ejercitoActual) {
-        cantidad = n;
-        ejercitos = ejercitoActualesI;
+        cantidad = n + 1;
     }
 }
